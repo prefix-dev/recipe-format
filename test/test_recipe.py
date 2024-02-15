8ccc1a46057fd2e26dfddfc838655cec0955a593
Yannik Tausch <yannik.tausch@quantco.com>
2024-02-15 14:58:30 +0100

    add tests for issue #12

--- test/test_recipe.py
+++ test/test_recipe.py
@@ -2,6 +2,8 @@ import json
 import pytest
 import yaml
 from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
 
 @pytest.fixture(
     scope="module",
@@ -11,12 +13,28 @@ from jsonschema import validate
         "zlib"
     ],
 )
-def recipe(request) -> str:
+def valid_recipe(request) -> str:
     recipe_name = request.param
-    with open(f"examples/{recipe_name}/recipe.yaml") as f:
+    with open(f"examples/valid/{recipe_name}/recipe.yaml") as f:
         recipe = f.read()
     recipe_yml = yaml.safe_load(recipe)
     return recipe_yml
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        "complex1",
+        "simple1",
+    ],
+)
+def invalid_recipe(request) -> str:
+    recipe_name = request.param
+    with open(f"examples/invalid/{recipe_name}.yaml") as f:
+        recipe = f.read()
+    recipe_yml = yaml.safe_load(recipe)
+    return recipe_yml
+
 
 @pytest.fixture()
 def recipe_schema():
@@ -25,5 +43,10 @@ def recipe_schema():
     return schema
 
 
-def test_recipe_schema(recipe_schema, recipe):
-    validate(instance=recipe, schema=recipe_schema)
+def test_recipe_schema_valid(recipe_schema, valid_recipe):
+    validate(instance=valid_recipe, schema=recipe_schema)
+
+
+def test_recipe_schema_invalid(recipe_schema, invalid_recipe):
+    with pytest.raises(ValidationError):
+        validate(instance=invalid_recipe, schema=recipe_schema)
